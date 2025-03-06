f80fd3325c527a9d5a71e4efb0fa12d0185edb3f
khaled961 <khaledalhoussein3@gmail.com>
2025-03-06 22:54:26 +0200

    .

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -1,27 +1,71 @@
-let theBiggest = ['bla', 'propaganda', 'other', 'aaa', 'battery', 'test'];
+let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
 
-
-let find = theBiggest.reduce((acc, curr) => {
-    return acc.length > curr.length ? acc : curr;
+// Elzero
+let str = mix.map((el) => {
+    return isNaN(parseInt(el)) ? el : ''
+}).reduce((acc, cur) => {
+    return acc + cur
 })
 
-console.log(find);
+console.log(str);
 
-let chars = ['e', '@', '@', 'l', 'z', 'e', 'r', '@', '0'];
 
-let ok = chars.filter((e) => {
-    return e !== '@'
+let myString = "EElllzzzzzzzeroo";
+
+// Elzero
+let noRepeat = myString.split('').filter((el, index) => {
+    return myString.indexOf(el) == index
+}).join('')
+
+console.log(noRepeat);
+
+
+let myArray = ["E", "l", "z", ["e", "r"], "o"];
+
+// Elzero
+let see = myArray.reduce((acc, cur) => {
+    return acc.concat(cur), []
+})
+
+console.log(see);
+
+
+
+let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
+
+// [-1, -10, 10, 20, -5, -3]
+
+let ss = numsAndStrings.filter((el) => {
+    return !isNaN(parseInt(el))
+}).map((e) => {
+    return -e
+})
+
+console.log(ss);
+
+
+let nums = [2, 12, 11, 5, 10, 1, 99];
+
+// 500
+
+
+let calc = nums.reduce((acc, curr) => {
+    return curr % 2 == 0 ? acc * curr : acc + curr
+}, 1)
+
+console.log(calc);
+
+
+let mySring = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let solution = mySring.split(',').map((e)=>{
+    return isNaN(parseInt(e)) ? e : ''
+}).filter((e)=>{
+    return e != '_'
 }).reduce((acc,curr)=>{
-    return acc + curr
+    return acc+curr
+}).split('').filter((el,index)=>{
+    return mySring.indexOf(el) !== index
 })
-console.log(ok);
 
-
-
-
-
-
-
-
-
-
+console.log(solution);
