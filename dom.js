f323470a06b3c1679f35482b9ffde79d10da4ce0
khaled961 <khaledalhoussein3@gmail.com>
2025-03-14 04:25:07 +0200

    dom challenge

--- dom.js
+++ dom.js
@@ -1,22 +1,36 @@
+let header = document.createElement('div');
+let brand = document.createElement('span');
+let navbar = document.createElement('div');
+let content = document.createElement('div');
+let footer = document.createElement('div')
 
+footer.style.cssText = 'background-color:#146542;text-align:center;color:white;padding:5px';
+content.style.cssText = 'background-color:#ececec;display:grid;grid-template-columns: auto auto auto;';
 
-document.querySelector('form').onsubmit = function (e) {
-    e.preventDefault();
+let footerDiv = document.createElement('p');
+footerDiv.innerHTML = 'Copyright';
 
-    let text = document.querySelector('[name=texts]').value;
-    let number = document.querySelector('[name=elements]').value;
-    let type = document.querySelector('select').value;
-    let container = document.querySelector('.results')
+footer.appendChild(footerDiv)
 
-    container.innerHTML = '';
-    
-    for (let i = 0; i < number; i++) {
-        let divsec = document.createElement(type);
-        divsec.classList.add('div-style')
-        const newContent = document.createTextNode(text);
-        divsec.appendChild(newContent)
-        container.appendChild(divsec)
-    }
-
-
+for (let i = 0; i < 15; i++) { 
+    let divContent = document.createElement('div');
+    divContent.style.cssText = 'background-color:white;padding:10px;margin:5px;border:1px solid black;text-align:center;display:grid;grid-template-columns: auto';
+    divContent.innerHTML = `Product ${i+1}`;
+    content.appendChild(divContent);
 }
+
+
+
+navbar.innerHTML = 'Home About Service Contact';
+header.style.cssText = 'background-color:grey;display:flex;justify-content:space-between;padding:10px;color:white';
+brand.innerHTML = 'Elzero';
+
+
+
+header.append(brand)
+header.append(navbar)
+document.body.append(header)
+document.body.append(content)
+document.body.append(footer)
+
+
