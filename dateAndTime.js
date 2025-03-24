ca063010b0c1d5c9c381f616f6da84b5df556282
khaled961 <khaledalhoussein3@gmail.com>
2025-03-25 00:37:41 +0200

    dates

---
+++ dateAndTime.js
@@ -0,0 +1,31 @@
+// // Needed Output
+// "1247939400 Seconds"
+// "20798990 Minutes"
+// "346650 Hours"
+// "14444 Days"
+// "481 Months"
+// "40 Years"
+
+// let date = new Date('11 17 1995')
+// let now = new Date()
+
+// let diff = now - date;
+
+// let sec = (diff/1000).toFixed();
+// let min = (sec/60).toFixed();
+// let hour = (min/60).toFixed();
+// let days = (hour/24).toFixed();
+// let years = (days/365).toFixed();
+// let months = (days/30).toFixed();
+
+
+// console.log(sec,min,hour,days,years,months);
+
+
+// ex2
+// Needed Output
+// "Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
+// let date = new Date(0);
+// date.setFullYear(date.getFullYear() +10);
+// date.setSeconds(date.getSeconds()+1)
+// console.log(date);
